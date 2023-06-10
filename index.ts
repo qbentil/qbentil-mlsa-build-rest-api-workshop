db1b817fe58843db18d8e926980564e4d8cb2703
Bentil Shadrack <bentilshadrack72@gmail.com>
2023-06-10 18:20:08 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,12 +3,13 @@ import Errorhandler from "./middleware/errorHandler"
 import TODOROUTE from "./routes/todo"
 import dotenv from "dotenv"
 import express from "express"
-
+import cors from 'cors'
 dotenv.config()
 
 // init APP
 const APP = express()
 APP.use(express.json())
+APP.use(cors({credentials: true, origin:true}));
 
 // Routes
 APP.use("/todo", TODOROUTE)
